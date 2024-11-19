827a8750f12b960502669231ab5ee53bc821c0d2
Purusottam Jena <purusottam.jena@athrvcloud.com>
2024-11-19 18:06:20 +0530

    Initial Commit

---
+++ include/my_math_function.h
@@ -0,0 +1,16 @@
+#ifndef MY_MATH_FUNCTION_H
+#define MY_MATH_FUNCTION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
